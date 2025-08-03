a8e83b8a747ff5168bde7f00a216aee7946afba0
Paulo Santos <GitPaulo@users.noreply.github.com>
2025-08-03 13:52:38 +0000

    updating yaml script globbing to be more versatile

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Linting YAML..."
-yamllint _config.yml _data/*.yml
+yamllint _config.yml .lefthook.yml _data/*.y{a,}ml .github/workflows/*.y{a,}ml
 
 echo "Linting Markdown..."
 markdownlint -c .markdownlint.json '**/*.md'
