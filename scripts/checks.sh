b0f824b97bae36c80dc3529472d110a87a48c530
Paulo Santos <GitPaulo@users.noreply.github.com>
2025-08-03 14:09:20 +0000

    exclude _site/ from markdownlint

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -5,7 +5,7 @@ echo "Linting YAML..."
 yamllint _config.yml .lefthook.yml _data/*.y{a,}ml .github/workflows/*.y{a,}ml
 
 echo "Linting Markdown..."
-markdownlint -c .markdownlint.json '**/*.md'
+markdownlint -c .markdownlint.json '**/*.md' --ignore '_site/**'
 
 echo "Building site..."
 bundle exec jekyll build --quiet
